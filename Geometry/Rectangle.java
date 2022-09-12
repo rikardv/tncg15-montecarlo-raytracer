bb71b357490e85b6d3f773a854d10d80a2be069d
Tim <tim.j.myllyla@gmail.com>
2022-09-12 16:12:08 +0200

    strcture

---
+++ Geometry/Rectangle.java
@@ -0,0 +1,9 @@
+package Geometry;
+import Utils.Vector3d;
+
+public class Rectangle {
+
+    Vector3d position = new Vector3d(0.0, 0.0, 0.0);
+
+
+}
---
+++ Utils/Vector3d.java
@@ -0,0 +1,13 @@
+package Vector3d;
+
+public class Vector3d {
+    double x;
+    double y;
+    double z;
+
+    public Vector3d(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
